24c8fa081ee63f39b8e472bef823ee7c1b27566a
Vitaliia <vitaliialymar@gmail.com>
2022-07-25 23:15:56 +0300

    add tests

---
+++ tests/capitalize.test.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line import/extensions
+import capitalize from '../src/capitalize.js';
+
+if (capitalize('hello') !== 'Hello') {
+  throw new Error('Функция работает неверно!');
+}
+
+if (capitalize('') !== '') {
+  throw new Error('Функция работает неверно!');
+}
+
+console.log('Все тесты пройдены!');
